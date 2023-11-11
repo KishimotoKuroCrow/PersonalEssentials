f4cf3dab97b89cabd56ab0b2dc27a7d2f1cc34f5
KishimotoKuroCrow <Kishimoto.BlackCoffee@gmail.com>
2023-11-11 13:52:06 -0500

    - Correction to variables, added extra path to set honor-http-proxy

--- SetProxy_wsudo.sh
+++ SetProxy_wsudo.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 PROXYURL=${1}
-CONF_PATH = "/etc/pacman.d/gnupg/"
-CONF_NAME = "dirmngr.conf"
 
 export http_proxy=${PROXYURL}
 export https_proxy=${PROXYURL}
 export ftp_proxy=${PROXYURL}
 export all_proxy=${PROXYURL}
 
+CONF_NAME="dirmngr.conf"
+CONF_PATH="/etc/pacman.d/gnupg/"
 mkdir -p ${CONF_PATH}
 echo honor-http-proxy > ${CONF_PATH}/${CONF_NAME}
 
+CONF_PATH="/etc/gnupg/"
+mkdir -p ${CONF_PATH}
+echo honor-http-proxy > ${CONF_PATH}/${CONF_NAME}
